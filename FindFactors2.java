027846bc5f6b65c72dbd855ccf5c010882994898
unknown <mayureshguhe@gmail.com>
2026-03-25 16:52:42 +0530

    Best Optimised way: Time Complexity- o(√n)

---
+++ FindFactors2.java
@@ -0,0 +1,35 @@
+// Best Optimised way: Time Complexity- o(√n)
+
+import java.util.Scanner;
+
+public class FindFactors2 {
+
+    static int factors(int num) {
+        int itr=0;
+        int count = 0;
+        for (int i = 1; i*i <= num; i++)      // Instead of i*i=num you can use Math.sqrt() 
+        {
+            itr++;
+            if (num % i == 0) {
+                if (i * i == num) {
+                    count = count + 1; // for ex: 10*10,20*20 increase only by one
+                } else {
+                    count = count + 2; // 2 iterations work in one
+                }
+            }
+
+        }
+        System.out.println("No. of iterations :"+itr);
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the No:");
+        int n = sc.nextInt();
+        int count = factors(n);
+        System.out.println("No. of factors: " + count);
+
+    }
+}
