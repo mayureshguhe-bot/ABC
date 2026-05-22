c694a3cebdaf440d5373985aa1f2116e5e967e9e
unknown <mayureshguhe@gmail.com>
2026-05-22 21:09:19 +0530

    A new program using PrefixSum(Brute-Force)

--- ArrayCore2web/PrefixSum1.java
+++ ArrayCore2web/PrefixSum1.java
@@ -8,19 +8,22 @@ For all queries, print sum of all elements from index s to e.
     Q : 3
     
     Queries    s      e        sum
-    query 1:   1      3         12
+    query 1:   1      3         12                                  
     query 1:   2      7         12
     query 1:   1      1         6
        */
+ 
+// Brute-Force Approach :-
 
 import java.util.*;
 
-public class PrefixSum {
+public class PrefixSum1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // int[] arr = new int[] { -3, 6, 2, 4, 5, 2, 8, -9, 3, 1 };
         // int N = 10;
         // int Q = 3;
+         
         System.out.println("Enter the size of Array:");
         int N = sc.nextInt();
         int[] arr= new int[N];
@@ -32,7 +35,7 @@ public class PrefixSum {
         System.out.println("Enter the no. of queries:  ");
         int Q=sc.nextInt();
         
-        for (int i = 1; i <= Q; i++) {
+        for (int i = 1; i <= Q; i++) {        // TC : O(Q)        
             int sum = 0;
             System.out.println();
             System.out.println("query "+i+":");
@@ -41,7 +44,7 @@ public class PrefixSum {
             System.out.print("Enter end index: ");
             int e = sc.nextInt();
 
-            for (int j = s; j <= e; j++) {      // O(n): Worst Case when user entered s=0 and e=N-1(Full Array)
+            for (int j = s; j <= e; j++) {      // O(N): Worst Case when user entered s=0 and e=N-1(Full Array)
                 sum = sum + arr[j];
             }
             System.out.println("Sum = " + sum);
@@ -51,4 +54,4 @@ public class PrefixSum {
 
 }
 
-//  Time Complexity: O(Q*N) not O(n^2)
+//  Time Complexity: O(Q*N) not O(N^2)
