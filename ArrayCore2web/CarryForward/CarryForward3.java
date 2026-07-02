c8adbb145b58d5bca8009bec74cd2eacde842605
unknown <mayureshguhe@gmail.com>
2026-07-02 16:10:28 +0530

    Optimised way for problem mentioned in CarryForward2

---
+++ ArrayCore2web/CarryForward/CarryForward3.java
@@ -0,0 +1,35 @@
+/* Given an integer array of size N.
+Build an Array leftMax of size N. leftmax i contains
+the maximum from the index 0 to index i.
+    Arr : [-3,6,2,4,5,2,8,-9,3,1]
+    N : 10
+    leftMax : [-3,6,6,6,6,6,8,8,8,8]
+ */
+// Optimised Approach:
+
+
+public class CarryForward3 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{-3,6,2,4,5,2,8,-9,3,1};
+        int n=10;
+        int leftMax[] = new int[n];
+
+        leftMax[0]=arr[0];
+
+        for(int i=1;i<n;i++){                // TC: O(n)
+            if(arr[i]>leftMax[i-1]){
+                leftMax[i]=arr[i];
+            }
+            else{
+                leftMax[i]=leftMax[i-1];
+            }
+        }
+
+        for(int x:leftMax){
+            System.out.print(x+" ");
+        }
+    }
+    
+}
+
+// Optimised Time Complexity: O(n)
