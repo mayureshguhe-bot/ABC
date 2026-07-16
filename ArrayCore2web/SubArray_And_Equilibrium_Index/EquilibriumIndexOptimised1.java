a054b514c25fed52b398b8c7783f5d9a3158587e
unknown <mayureshguhe@gmail.com>
2026-07-16 14:06:56 +0530

    Optimised Solution for Equilibrium Index Problem also more optimised version exist having space complexity O(1) solving in next few days

--- ArrayCore2web/SubArray_And_Equilibrium_Index/EquilibriumIndexOptimised1.java
+++ ArrayCore2web/SubArray_And_Equilibrium_Index/EquilibriumIndexOptimised1.java
@@ -8,16 +8,16 @@ public class EquilibriumIndexOptimised1 {
         RpfArr[arr.length - 1] = arr[arr.length - 1];
         int flag = 0;
 
-        for (int i = 1; i < arr.length; i++) {
-            LpfArr[i] = LpfArr[i - 1] + arr[i];
+        for (int i = 1; i < arr.length; i++) {            // TC : O(N)
+            LpfArr[i] = LpfArr[i - 1] + arr[i];           // SC : O(N)
         }
-        for (int i = arr.length - 2; i >= 0; i--) {
-            RpfArr[i] = RpfArr[i + 1] + arr[i];
+        for (int i = arr.length - 2; i >= 0; i--) {       // TC : O(N)
+            RpfArr[i] = RpfArr[i + 1] + arr[i];           // SC : O(N)
         }
 
         RpfArr[arr.length] = 0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {           // TC : O(N)
             if (i == 0 && RpfArr[i + 1] == 0){
                 flag=1;
                 System.out.println("Equilibrium Index: " + i);
@@ -37,3 +37,6 @@ if (LpfArr[i - 1] == RpfArr[i + 1]) {
 
     }
 }
+
+// Total Time Complexity: O(N)+O(N)+O(N)=O(3N)==O(N)
+// Space Complexity : O(N)
