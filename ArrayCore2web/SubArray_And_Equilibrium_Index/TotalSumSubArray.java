7859acee730f976e0f7268d99924553ded97074c
unknown <mayureshguhe@gmail.com>
2026-07-19 15:15:41 +0530

    Program Printing Sum of all Subarrays together.

---
+++ ArrayCore2web/SubArray_And_Equilibrium_Index/TotalSumSubArray.java
@@ -0,0 +1,19 @@
+public class TotalSumSubArray {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 2, 4, 1, 3 };
+
+        int totalSum=0;
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                totalSum+=sum;
+                System.out.println(sum);
+            }
+            
+        }
+        System.out.println(totalSum);
+
+    }
+    
+}
