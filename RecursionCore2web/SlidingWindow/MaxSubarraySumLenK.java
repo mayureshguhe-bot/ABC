df846f6c78172e7f592040a39f581fca0a804d8b
unknown <mayureshguhe@gmail.com>
2026-08-11 15:50:10 +0530

    Program finding max subarray sum of fixed length k(Brute-Force)

---
+++ RecursionCore2web/SlidingWindow/MaxSubarraySumLenK.java
@@ -0,0 +1,33 @@
+package RecursionCore2web.SlidingWindow;
+
+/* Given an array of size N.Find the maximum subarray sum of length k.
+     Input : [-3,4,-2,5,3,-2,8,2,1,4]
+     k : 4
+     Output : 15
+ */
+
+public class MaxSubarraySumLenK {
+    public static void main(String[] args) {
+        int[] arr = new int[] { -3, 4, -2, 5, 3, -2, 8, 2, 1, 4 };
+        int k = 4;
+
+        int start = 0;
+        int end = k - 1;
+        int maxEle = Integer.MIN_VALUE;
+
+        while (end < arr.length) {
+            int sum = 0;
+
+            for (int i = start; i <= end; i++) {
+                sum += arr[i];
+            }
+            if (sum > maxEle)
+                maxEle = sum;
+
+            start++;
+            end++;
+
+        }
+        System.out.println(maxEle);
+    }
+}
