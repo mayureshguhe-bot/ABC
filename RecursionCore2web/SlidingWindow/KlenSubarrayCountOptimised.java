bae2160a8ae34c40d6fe693326cce244cfe37bf0
unknown <mayureshguhe@gmail.com>
2026-08-11 14:56:39 +0530

    Sliding Window Examples

---
+++ RecursionCore2web/SlidingWindow/KlenSubarrayCountOptimised.java
@@ -0,0 +1,11 @@
+package RecursionCore2web.SlidingWindow;
+
+public class KlenSubarrayCountOptimised {
+        public static void main(String[] args) {
+        int[] arr = new int[]{-3,4,-2,5,3,-2,8,2,1,4};
+        int k = 4;
+
+        System.out.println(arr.length-k+1);
+
+    }
+}
