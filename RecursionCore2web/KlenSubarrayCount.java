341abb06a5b011023f003466c8a2cfe7e83aed0e
unknown <mayureshguhe@gmail.com>
2026-08-11 14:46:05 +0530

    Counting the no. of subarrays for fixed length before that we haven't done for fixed size.

--- RecursionCore2web/KlenSubarrayCount.java
+++ RecursionCore2web/KlenSubarrayCount.java
@@ -1,6 +1,9 @@
 package RecursionCore2web;
-
-
+/* Count the number of subarray for a fixed length k.
+     Input : [-3,4,-2,5,3,-2,8,2,1,4]
+     k : 4
+     Output: 7
+*/
 
 public class KlenSubarrayCount {
     public static void main(String[] args) {
@@ -8,7 +11,7 @@ public class KlenSubarrayCount {
         int k = 4;
         int count =0;
 
-        int start = 0;    // it is also not needed!
+        int start = 0;       // it is also not needed!
         int end = k-1;
         
         while(end < arr.length){
