b40785becab6dc46888e893c2eb35d97d4324ca8
unknown <mayureshguhe@gmail.com>
2026-08-12 21:01:44 +0530

    Program finding max subarray sum of fixed length k(Optimised 2: Sliding Window )

---
+++ RecursionCore2web/SlidingWindow/MaxSubarraySumLenKOptimised_SlidingWindow.java
@@ -0,0 +1,41 @@
+/* More Optimised Version : Sliding Window(we are just sliding fixed size subarray(window) forward.)
+* [[-3, [4, -2, 5], 3], -2, 8, 2, 1, 4]
+*          |_>repeating window(Fixed size subarray)
+*   -3+4+(-2)+5= 4 , 4+(-2)+5+3= 10
+*    instead of: sum - arr[1st] + arr[last of second subarray]
+*                   4 - (-3) + 3 = 10
+*/
+package RecursionCore2web.SlidingWindow;
+
+public class MaxSubarraySumLenKOptimised_SlidingWindow {
+    public static void main(String[] args) {
+        int[] arr = new int[]{-3, 4, -2, 5, 3, -2, 8, 2, 1, 4};
+        int k =4;
+
+        int start = 0;
+        int end=k-1;
+
+        int sum = 0;
+        for(int i = start;i<=end;i++){
+            sum+=arr[i];
+        }
+
+         start = 1;
+         end = k;
+         int maxEle = Integer.MIN_VALUE;
+
+        while(end<arr.length){
+            sum = sum - arr[start-1] + arr[end];
+
+            if(sum>maxEle){
+                maxEle = sum;
+            }
+            start++;
+            end++;
+        }
+
+        System.out.println(maxEle);
+
+    }
+    
+}
