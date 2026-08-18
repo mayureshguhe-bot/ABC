ed2f6fd44e7e378a80fd9b90c4e4ba41ae98e52c
unknown <mayureshguhe@gmail.com>
2026-08-18 21:26:12 +0530

    Program printing Sum of n numbers using recursion

--- RecursionCore2web/Recursion/SumOfN_Recursion.java
+++ RecursionCore2web/Recursion/SumOfN_Recursion.java
@@ -1,27 +1,28 @@
 public class SumOfN_Recursion {
 
-// Using For-Loop:-
-    int add(int n){
-        int sum=0;
-    for(int i=1;i<=n;i++){
-        sum+=i;
-    }
-    return sum;
+    // Using For-Loop:-
+    int add(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
-// Using Recursion:-
-    int addR(int n){
-        if(n==0)
+    // Using Recursion:-
+    int addR(int n) {
+        if (n == 0)
             return 0;
         n = n + addR(--n);
         return n;
     }
+
     public static void main(String[] args) {
         SumOfN_Recursion obj = new SumOfN_Recursion();
-       int sumF= obj.add(5);
-       int sumR= obj.addR(5);
-       System.out.println(sumF);
-       System.out.println(sumR);
-        
-    }    
+        int sumF = obj.add(5);
+        int sumR = obj.addR(5);
+        System.out.println(sumF);
+        System.out.println(sumR);
+
+    }
 }
