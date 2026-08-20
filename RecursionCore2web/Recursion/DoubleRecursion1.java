cc7eae49d0e8ca6f343fdd969ca265cdceb3246a
unknown <mayureshguhe@gmail.com>
2026-08-20 21:05:37 +0530

    Program showing : Double Recursion

--- RecursionCore2web/Recursion/DoubleRecursion1.java
+++ RecursionCore2web/Recursion/DoubleRecursion1.java
@@ -1,11 +1,12 @@
 public class DoubleRecursion1 {
-    void fun(int num){
-        if(num <= 1)
+    void fun(int num) {
+        if (num <= 1)
             return;
-     fun(num-2); 
-     System.out.println(num);
-     fun(num-1);
+        fun(num - 2);
+        System.out.println(num);
+        fun(num - 1);
     }
+
     public static void main(String[] args) {
         DoubleRecursion1 obj = new DoubleRecursion1();
         obj.fun(5);
