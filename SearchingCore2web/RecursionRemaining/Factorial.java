b69a858524cc5de93fbb08002f3fc75435f071a1
unknown <mayureshguhe@gmail.com>
2026-08-26 19:13:07 +0530

    Remaining Part

--- SearchingCore2web/RecursionRemaining/Factorial.java
+++ SearchingCore2web/RecursionRemaining/Factorial.java
@@ -2,23 +2,24 @@ package SearchingCore2web.RecursionRemaining;
 
 public class Factorial {
 
-// Using For Loop:
-    int fact(int num){
-        int f=1;
-        for(int i =1;i<=num;i++){
-            f=f*i;
+    // Using For Loop:
+    int fact(int num) {
+        int f = 1;
+        for (int i = 1; i <= num; i++) {
+            f = f * i;
         }
         return f;
     }
 
-// Using Recursion
-    int factR(int num){
-        if(num==0)
+    // Using Recursion
+    int factR(int num) {
+        if (num == 0)
             return 1;
-        return num * factR(num-1);
+        return num * factR(num - 1);
     }
+
     public static void main(String[] args) {
-       Factorial obj =new Factorial();
+        Factorial obj = new Factorial();
         int ret = obj.fact(2);
         int ret1 = obj.factR(2);
         System.out.println(ret);
