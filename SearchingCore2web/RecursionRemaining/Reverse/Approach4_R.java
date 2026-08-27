6b79d8d06b8f6c34785b05002ebaffe7c7deaa5e
unknown <mayureshguhe@gmail.com>
2026-08-27 14:52:40 +0530

    Just Writing

--- SearchingCore2web/RecursionRemaining/Reverse/Approach4_R.java
+++ SearchingCore2web/RecursionRemaining/Reverse/Approach4_R.java
@@ -4,15 +4,16 @@ package SearchingCore2web.RecursionRemaining.Reverse;
 
 public class Approach4_R {
 
-    String reverse(String s){
-        if(s==null || s.length()<=1){
+    String reverse(String s) {
+        if (s == null || s.length() <= 1) {
             return s;
         }
-        return reverse(s.substring(1))+s.charAt(0);
+        return reverse(s.substring(1)) + s.charAt(0);
     }
+
     public static void main(String[] args) {
         Approach4_R obj = new Approach4_R();
-        String str= obj.reverse("jay");
+        String str = obj.reverse("Core2Web");
         System.out.println(str);
     }
 }
